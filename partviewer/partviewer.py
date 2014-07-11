1eaf5fb79eb4f4b74e6602adf3a507a814a315a7
Hazen Babcock <HazenBabcock@github.com>
2014-07-11 08:32:27 -0400

    Get part rotation and translation working (more or less..).

--- partviewer/partviewer.py
+++ partviewer/partviewer.py
@@ -7,6 +7,8 @@
 # Hazen 06/14
 #
 
+import math
+import numpy
 import sys
 
 from OpenGL import GL, GLU
@@ -25,12 +27,19 @@ class GLWidget(QtOpenGL.QGLWidget):
         QtOpenGL.QGLWidget.__init__(self, parent)
 
         self.last_pos = QtCore.QPoint()
+        self.offset = numpy.array([[0], [0], [0], [0]], dtype = numpy.float32)
+        self.p_matrix = None                                                     # Projection matrix.
         self.part = None
-        self.scale = 0.05
+        self.m_r_matrix = numpy.identity(4, dtype = numpy.float32)               # Model rotation matrix.
+        self.m_s_matrix = numpy.identity(4, dtype = numpy.float32)*0.01          # Model scale matrix.
+        self.m_t_matrix = numpy.identity(4, dtype = numpy.float32)               # Model translation matrix.
+        self.v_matrix = None                                                     # View matrix.
+        self.v_r_matrix = numpy.identity(4, dtype = numpy.float32)               # View rotation matrix.
         self.verbose = True
-        self.x_rotation = 0.0
-        self.y_rotation = 0.0
-        self.z_rotation = 0.0
+        #self.x_offset = 0.0
+        #self.y_offset = 0.0
+
+        self.m_s_matrix[3,3] = 1.0
 
         self.setMinimumSize(500, 500)
 
@@ -49,8 +58,8 @@ class GLWidget(QtOpenGL.QGLWidget):
         #GL.glEnable(GL.GL_LIGHT0)
         #GL.glEnable(GL.GL_DEPTH_TEST)
 
-        #self.loadPart("C:/Program Files (x86)/LDraw/parts/1.dat")
-        self.loadPart("../test/test3.dat")
+        self.loadPart("C:/Program Files (x86)/LDraw/parts/1.dat")
+        #self.loadPart("../test/test3.dat")
 
     def loadPart(self, filename):
         self.part = glParser.GLParser(None, [1.0, 0.0, 0.0], [0.0, 0.0, 0.0])
@@ -63,13 +72,10 @@ class GLWidget(QtOpenGL.QGLWidget):
         dx = event.x() - self.lastPos.x()
         dy = event.y() - self.lastPos.y()
 
-        gain = 4.0
-        if (event.buttons() and QtCore.Qt.LeftButton):
-            self.x_rotation += gain * dy
-            self.y_rotation += gain * dx
-        elif (event.buttons() and QtCore.Qt.RightButton):
-            self.x_rotation += gain * dy
-            self.z_rotation += gain * dx
+        if (event.buttons() == QtCore.Qt.LeftButton):
+            self.rotateView(dx, dy)
+        elif (event.buttons() == QtCore.Qt.RightButton):
+            self.translateView(dx, dy)
 
         self.lastPos = event.pos()
         self.updateGL()
@@ -78,33 +84,68 @@ class GLWidget(QtOpenGL.QGLWidget):
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
 
         if self.part is not None:
-            #GL.glMatrixMode(GL.GL_MODELVIEW)
-            #GL.glLoadIdentity()
-            #GL.glPushMatrix()
-            #GL.glTranslatef(0.0, 0.0, -50.0)
-            #GL.glScalef(self.scale, self.scale, self.scale);
-            #GL.glRotatef(self.x_rotation * 0.1, 1.0, 0.0, 0.0)
-            #GL.glRotatef(self.y_rotation * 0.1, 0.0, 1.0, 0.0)
-            #GL.glRotatef(self.z_rotation * 0.1, 0.0, 0.0, 1.0)
-            
-            self.part.render()
-            #GL.glPopMatrix()
+            m = numpy.dot(self.m_s_matrix, numpy.dot(self.m_t_matrix, self.m_r_matrix))
+            mvp = numpy.dot(m, numpy.dot(self.v_matrix, self.p_matrix))
+            self.part.render(mvp)
 
         GL.glFlush()
 
     def resizeGL(self, w, h):
         GL.glViewport(0, 0, w, h)
-
+        
+        # Use OpenGL to figure out the projection matrix for us.
         GL.glMatrixMode(GL.GL_PROJECTION)
         GL.glLoadIdentity()
-        GLU.gluPerspective(60.0, 1.0, 0.01, 100.0)
+        GLU.gluPerspective(20.0, float(w)/float(h), 0.1, 10.0)
+        self.p_matrix = numpy.array(GL.glGetDoublev(GL.GL_PROJECTION_MATRIX),
+                                    dtype = numpy.float32)
 
+        self.updateView()
+
+    def rotateView(self, dx, dy):
+        dx = -dx/50.0
+        dy = dy/50.0
+        rot_x = numpy.matrix([[math.cos(dx), 0, math.sin(dx), 0],
+                              [0, 1, 0, 0],
+                              [-math.sin(dx), 0, math.cos(dx), 0],
+                              [0, 0, 0, 1]],
+                           dtype = numpy.float32)
+        rot_y = numpy.matrix([[1, 0, 0, 0],
+                              [0, math.cos(dy), math.sin(dy), 0],
+                              [0, -math.sin(dy), math.cos(dy), 0],
+                              [0, 0, 0, 1]],
+                             dtype = numpy.float32)
+        self.v_r_matrix = numpy.dot(self.v_r_matrix, numpy.dot(rot_x, rot_y))
+        self.updateView()
+
+    def translateView(self, dx, dy):
+        gain = 0.005
+        temp = numpy.array([[-gain * dx], [gain * dy], [0], [0]], dtype = numpy.float32)
+        self.offset += numpy.dot(self.v_r_matrix, temp)
+        self.updateView()
+
+    def updateView(self):
+        eye_pos = numpy.array([[0], [0], [5], [0]], dtype = numpy.float32)  # Eye location.
+        eye_pos = numpy.dot(self.v_r_matrix, eye_pos)
+
+        up = numpy.array([[0], [1], [0], [0]], dtype = numpy.float32)  # Up vector.
+        up = numpy.dot(self.v_r_matrix, up)
+
+        GL.glLoadIdentity()
+        GLU.gluLookAt(self.offset[0,0] + eye_pos[0,0], self.offset[1,0] + eye_pos[1,0], self.offset[2,0] + eye_pos[2,0],
+                      self.offset[0,0], self.offset[1,0], self.offset[2,0],
+                      up[0,0], up[1,0], up[2,0])
+        self.v_matrix = numpy.array(GL.glGetDoublev(GL.GL_PROJECTION_MATRIX),
+                                    dtype = numpy.float32)
+        
     def wheelEvent(self, event):
         gain = 1.2
         if (event.delta() > 0):
-            self.scale = self.scale * gain
+            for i in range(3):
+                self.m_s_matrix[i,i] = self.m_s_matrix[i,i] * gain
         else:
-            self.scale = self.scale * (1.0/gain)
+            for i in range(3):
+                self.m_s_matrix[i,i] = self.m_s_matrix[i,i] / gain
         self.updateGL()
 
 
