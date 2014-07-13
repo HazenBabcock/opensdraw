f654d0e0eb8240be68ba33262a86546a4230d69a
Hazen Babcock <HazenBabcock@github.com>
2014-07-12 23:20:15 -0400

    Add license.

--- lcad_lib/datFileParser.py
+++ lcad_lib/datFileParser.py
@@ -2,9 +2,10 @@
 #
 ## @file
 #
-# Parse a dat file.
+# Parse a dat file as described here:
+# http://www.ldraw.org/article/218.html
 #
-# Hazen 06/14
+# Hazen 07/14
 #
 
 import os
@@ -194,3 +195,26 @@ if (__name__ == "__main__"):
     if 1:
         parsePartFile(Parser(True, True), "c:/Program Files (x86)/LDraw/parts/15.dat")
 
+#
+# The MIT License
+#
+# Copyright (c) 2014 Hazen Babcock
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
