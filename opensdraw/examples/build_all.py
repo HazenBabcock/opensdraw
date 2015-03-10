1ec66ff6a27c6c2d43907613faae1809869ac7f3
Hazen Babcock <hbabcock@mac.com>
2015-03-09 21:12:54 -0400

    Initial commit. This builds all of the examples.

---
+++ opensdraw/examples/build_all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""
+.. module:: build_all
+   :synopsis: Builds all the examples.
+
+.. moduleauthor:: Hazen Babcock
+"""
+
+import os
+import subprocess
+
+
+examples = ["auto-step.lcad",
+            "chain.lcad",
+            "curve.lcad",
+            "dumper-truck.lcad",
+            "gears.lcad",
+            "gripper.lcad",
+            "rib-hose.lcad",
+            "steps.lcad",
+            "wall.lcad"]
+
+builder = "../scripts/lcad_to_ldraw.py"
+
+for example in examples:
+    proc_params = ["python",
+                   builder,
+                   example]
+    print "Building:", example
+    subprocess.call(proc_params)
+    print ""
+
