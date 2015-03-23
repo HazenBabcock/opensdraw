14ba80d874017cefcba65626a42a1a9e2aedd471
Hazen Babcock <hbabcock@mac.com>
2015-03-23 07:58:41 -0400

    Add tube() function for creating tubes. Use dynamic step size to adjust the number of primitives based on how curved the part is.

--- opensdraw/library/shapes.py
+++ opensdraw/library/shapes.py
@@ -19,6 +19,22 @@ import opensdraw.lcad_language.parts as parts
 lcad_functions = {}
 
 
+#
+# Helper functions.
+#
+
+def createRing(matrix, vectors):
+    ring = []
+    for vec in vectors:
+        ring.append(numpy.dot(matrix, vec)[:3])
+    return ring
+
+def createVectors(matrices, vector):
+    vectors = [vector]
+    for mm in matrices:
+        vectors.append(numpy.dot(mm, vector))
+    return vectors
+
 def rotationMatrices():
     matrices = []
     d_angle = math.radians(22.5)
@@ -33,11 +49,79 @@ def rotationMatrices():
         angle += d_angle
     return matrices
 
-
 def transformMatrix(posori):
     return numpy.dot(geometryFunctions.translationMatrix(*posori[0:3]),
                      angles.rotationMatrix(*posori[3:6]))
 
+
+#
+# Helper classes.
+#
+class Stepper(object):
+    """
+    Handles dynamically stepping along the curve 
+    to minimize the number of sub-sections.
+    """
+    def __init__(self, curve, start, stop):
+        self.curve = curve
+        self.pos = start
+        self.step = 16
+        self.stop = stop
+
+        self.pos_ori = curve.getPosOrientation(start)
+
+    def anglesDiffer(self, new_pos_ori):
+        diff = 0
+        for i in range(3,6):
+            diff += abs(self.pos_ori[i] - new_pos_ori[i])
+        if (diff > 6):
+            return True
+        else:
+            return False
+
+    # For testing.
+    def _nextPos(self):
+        self.pos += 4
+        self.pos_ori = self.curve.getPosOrientation(self.pos)
+        return self.pos
+
+    def nextPos(self):
+        cur = self.pos + self.step
+        new_pos_ori = self.curve.getPosOrientation(cur)
+        if self.anglesDiffer(new_pos_ori):
+            while (self.step > 1) and self.anglesDiffer(new_pos_ori):
+                self.step = self.step/2
+                cur = self.pos + self.step
+                new_pos_ori = self.curve.getPosOrientation(cur)
+            self.pos = cur
+            self.pos_ori = new_pos_ori
+            return self.pos
+        else:
+            while (cur < self.stop) and not self.anglesDiffer(new_pos_ori):
+                if (self.step < 32):
+                    self.step = self.step * 2
+                cur += self.step
+                new_pos_ori = self.curve.getPosOrientation(cur)
+            if (cur > self.stop):
+                self.pos = self.stop
+                self.pos_ori = self.curve.getPosOrientation(self.pos)
+                return self.pos
+            elif (self.anglesDiffer(new_pos_ori)):
+                self.pos = cur - self.step
+                self.pos_ori = self.curve.getPosOrientation(self.pos)
+                return self.pos
+            else:
+                self.pos = cur
+                self.pos_ori = new_pos_ori
+                return self.pos
+
+    def posOri(self):
+        return self.pos_ori
+
+
+#
+# Opensdraw functions.
+#
 
 class Axle(functions.LCadFunction):
     """
@@ -45,7 +129,7 @@ class Axle(functions.LCadFunction):
     
     :param curve: The curve that the axle should follow.
     :param start: The starting point on the curve.
-    :param stop: The ending point on the curve.
+    :param stop: The stopping point on the curve.
 
     The axle will have the color 16.
 
@@ -84,36 +168,30 @@ class Axle(functions.LCadFunction):
         
         group = model.curGroup()
         matrix = group.matrix()
+        stepper = Stepper(curve, start, stop)
 
-        lastv = []
-        cm = numpy.dot(matrix, transformMatrix(curve.getPosOrientation(start)))
-        for vec in self.vectors:
-            lastv.append(numpy.dot(cm, vec)[:3])
+        cm = numpy.dot(matrix, transformMatrix(stepper.posOri()))
+        lastv = createRing(cm, self.vectors)
         n_vert = len(lastv) - 1
 
-        d_pos = 4.0
-        pos = start + d_pos
+        pos = stepper.nextPos()
         while (pos < stop):
-            cm = numpy.dot(matrix, transformMatrix(curve.getPosOrientation(pos)))
-            curv = []
-            for vec in self.vectors:
-                curv.append(numpy.dot(cm, vec)[:3])
+            cm = numpy.dot(matrix, transformMatrix(stepper.posOri()))
+            curv = createRing(cm, self.vectors)
             for i in range(n_vert):
                 group.addPart(parts.Line(None, numpy.append(lastv[i], curv[i]), 16), True)
                 group.addPart(parts.Triangle(None, numpy.append(lastv[i], [lastv[i+1], curv[i]]), 16), True)
                 group.addPart(parts.Triangle(None, numpy.append(lastv[i+1], [curv[i+1], curv[i]]), 16), True)
-            lastv = curv
-            pos += d_pos
 
-        if (pos != stop):
-            cm = numpy.dot(matrix, transformMatrix(curve.getPosOrientation(stop)))
-            curv = []
-            for vec in self.vectors:
-                curv.append(numpy.dot(cm, vec)[:3])
-            for i in range(n_vert):
-                group.addPart(parts.Line(None, numpy.append(lastv[i], curv[i]), 16), True)
-                group.addPart(parts.Triangle(None, numpy.append(lastv[i], [lastv[i+1], curv[i]]), 16), True)
-                group.addPart(parts.Triangle(None, numpy.append(lastv[i+1], [curv[i+1], curv[i]]), 16), True)
+            pos = stepper.nextPos()
+            lastv = curv
+
+        cm = numpy.dot(matrix, transformMatrix(curve.getPosOrientation(stop)))
+        curv = createRing(cm, self.vectors)
+        for i in range(n_vert):
+            group.addPart(parts.Line(None, numpy.append(lastv[i], curv[i]), 16), True)
+            group.addPart(parts.Triangle(None, numpy.append(lastv[i], [lastv[i+1], curv[i]]), 16), True)
+            group.addPart(parts.Triangle(None, numpy.append(lastv[i+1], [curv[i+1], curv[i]]), 16), True)
 
 lcad_functions["axle"] = Axle()
 
@@ -157,7 +235,7 @@ class Ring(functions.LCadFunction):
 
         p1 = numpy.dot(m1, v1)
         p2 = numpy.dot(m2, v2)
-        if ccw is interpreter.lcad_t:
+        if functions.isTrue(ccw):
             for mz in self.matrices:            
                 p3 = numpy.dot(m1, numpy.dot(mz, v1))
                 p4 = numpy.dot(m2, numpy.dot(mz, v2))
@@ -176,3 +254,86 @@ class Ring(functions.LCadFunction):
 
 lcad_functions["ring"] = Ring()
 
+
+class Tube(functions.LCadFunction):
+    """
+    **tube** - Draw a tube using LDraw primitives.
+
+    :param curve: The curve that the tube should follow.
+    :param start: The starting point on the curve.
+    :param stop: The stopping point on the curve.
+    :param inner_radius: The inner radius of the tube.
+    :param outer_radius: The outer radius of the tube.
+
+    The tube will have the color 16.
+
+    Usage::
+
+     (tube curve 0 10 2 3) ; Draw a 2 LDU inner diameter, 3 LDU outer diameter 
+                           ; tube from 0 to 10 along curve.
+
+    """
+    def __init__(self):
+        functions.LCadFunction.__init__(self, "tube")
+
+        self.matrices = rotationMatrices()
+        self.setSignature([[functions.LCadFunction],
+                           [numbers.Number],
+                           [numbers.Number],
+                           [numbers.Number],
+                           [numbers.Number]])
+
+    def call(self, model, tree):
+        [curve, start, stop, inner_radius, outer_radius] = self.getArgs(model, tree)
+
+        group = model.curGroup()
+        matrix = group.matrix()
+        stepper = Stepper(curve, start, stop)
+
+        # Create vectors.
+        inner_vecs = createVectors(self.matrices, numpy.array([inner_radius, 0, 0, 1]))
+        outer_vecs = createVectors(self.matrices, numpy.array([outer_radius, 0, 0, 1]))
+        
+        # Starting ring.
+        cm = numpy.dot(matrix, transformMatrix(stepper.posOri()))
+        last_inner = createRing(cm, inner_vecs)
+        last_outer = createRing(cm, outer_vecs)
+
+        n_vert = len(last_inner) - 1
+        pos = stepper.nextPos()
+        while (pos < stop):
+            cm = numpy.dot(matrix, transformMatrix(stepper.posOri()))
+            cur_inner = createRing(cm, inner_vecs)
+            cur_outer = createRing(cm, outer_vecs)
+
+            for i in range(n_vert):
+
+                # Inner wall.
+                group.addPart(parts.Triangle(None, numpy.append(last_inner[i+1], [last_inner[i], cur_inner[i]]), 16), True)
+                group.addPart(parts.Triangle(None, numpy.append(last_inner[i+1], [cur_inner[i], cur_inner[i+1]]), 16), True)
+
+                # Outer wall.
+                group.addPart(parts.Line(None, numpy.append(last_outer[i], cur_outer[i]), 16), True)
+                group.addPart(parts.Triangle(None, numpy.append(last_outer[i], [last_outer[i+1], cur_outer[i]]), 16), True)
+                group.addPart(parts.Triangle(None, numpy.append(last_outer[i+1], [cur_outer[i+1], cur_outer[i]]), 16), True)
+
+            pos = stepper.nextPos()
+            last_inner = cur_inner
+            last_outer = cur_outer
+
+        cm = numpy.dot(matrix, transformMatrix(curve.getPosOrientation(stop)))
+        cur_inner = createRing(cm, inner_vecs)
+        cur_outer = createRing(cm, outer_vecs)
+
+        for i in range(n_vert):
+
+            # Inner wall.
+            group.addPart(parts.Triangle(None, numpy.append(last_inner[i+1], [last_inner[i], cur_inner[i]]), 16), True)
+            group.addPart(parts.Triangle(None, numpy.append(last_inner[i+1], [cur_inner[i], cur_inner[i+1]]), 16), True)
+
+            # Outer wall.
+            group.addPart(parts.Line(None, numpy.append(last_outer[i], cur_outer[i]), 16), True)
+            group.addPart(parts.Triangle(None, numpy.append(last_outer[i], [last_outer[i+1], cur_outer[i]]), 16), True)
+            group.addPart(parts.Triangle(None, numpy.append(last_outer[i+1], [cur_outer[i+1], cur_outer[i]]), 16), True)
+
+lcad_functions["tube"] = Tube()
