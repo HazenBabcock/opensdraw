01203d4bbae11e85d6994b2cc15b29217f430553
Hazen Babcock <hbabcock@mac.com>
2015-03-24 07:17:10 -0400

    Initial commit.

---
+++ opensdraw/lcad_language/curveFunctions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+.. module:: curveFunctions
+   :synopsis: The curve function.
+
+.. moduleauthor:: Hazen Babcock
+"""
+
+import numbers
+
+import functions
+import interpreter as interp
+import lcadTypes
+
+
+class CurveFunction(functions.LCadFunction):
+    """
+    The functions chain(), curve() and spring() all return the function
+    so that they can be used interchangeably.
+    """
+    def __init__(self, curve, name):
+        functions.LCadFunction.__init__(self, name)
+        self.setSignature([[lcadTypes.LCadObject, numbers.Number]])
+        self.curve = curve
+
+    def call(self, model, tree):
+        arg = self.getArg(model, tree, 0)
+
+        # If arg is t return the curve length.
+        if not isinstance(arg, numbers.Number):
+            if functions.isTrue(arg):
+                return self.curve.getLength()
+            else:
+                return interp.lcad_nil
+
+        # Return position and orientation.
+        return self.curve.getCoords(arg)
+
+    # This is to make it easier to call directly from other Python modules.
+    def getPosOrientation(self, pos):
+        return self.curve.getCoords(pos)
+
