9a66ad6ddb1779d98da4adc9e9c6adb653f466a2
Hazen Babcock <hbabcock@mac.com>
2015-04-21 09:10:29 -0400

    Add other viewing angles.

--- opensdraw/scripts/ldview_render.py
+++ opensdraw/scripts/ldview_render.py
@@ -25,6 +25,14 @@ options = ["-DefaultMatrix=-0.861209,0.362189,0.356561,0.455131,0.861824,0.22385
            "-SaveHeight=480",
            "-FOV=10"]
 
+# This is a better angle for the crane animation.
+options[0] = "-DefaultMatrix=-0.921756,0.386996,-0.0251828,-0.0175983,-0.106619,-0.994136,-0.387407,-0.915901,0.105077"
+options[1] = "-DefaultZoom=0.985235"
+
+# This is a better angle for the suspension animation.
+options[0] = "-DefaultMatrix=0.681231,-0.731242,-0.034584,-0.146912,-0.090277,-0.984997,0.71715,0.676115,-0.168931"
+options[1] = "-DefaultZoom=1"
+
 for dat_file in sorted(glob.glob(sys.argv[1] + "*.mpd")):
     print "Processing:", dat_file
     proc_params = ["LDView", 
