e7f722fa463562b9e97dba01bacd3e0bd1e5519a
Hazen Babcock <hbabcock@mac.com>
2015-05-11 07:46:46 -0400

    Add angles for power functions cable animation.

--- opensdraw/scripts/ldview_render.py
+++ opensdraw/scripts/ldview_render.py
@@ -26,12 +26,19 @@ options = ["-DefaultMatrix=-0.861209,0.362189,0.356561,0.455131,0.861824,0.22385
            "-FOV=10"]
 
 # This is a better angle for the crane animation.
-options[0] = "-DefaultMatrix=-0.921756,0.386996,-0.0251828,-0.0175983,-0.106619,-0.994136,-0.387407,-0.915901,0.105077"
-options[1] = "-DefaultZoom=0.985235"
+if 0:
+    options[0] = "-DefaultMatrix=-0.921756,0.386996,-0.0251828,-0.0175983,-0.106619,-0.994136,-0.387407,-0.915901,0.105077"
+    options[1] = "-DefaultZoom=0.985235"
 
 # This is a better angle for the suspension animation.
-options[0] = "-DefaultMatrix=0.681231,-0.731242,-0.034584,-0.146912,-0.090277,-0.984997,0.71715,0.676115,-0.168931"
-options[1] = "-DefaultZoom=1"
+if 0:
+    options[0] = "-DefaultMatrix=0.681231,-0.731242,-0.034584,-0.146912,-0.090277,-0.984997,0.71715,0.676115,-0.168931"
+    options[1] = "-DefaultZoom=1"
+
+# This is a better angle for the power functions cable animation.
+if 1:
+    options[0] = "-DefaultMatrix=-0.209952,0.0763675,-0.974724,-0.0662947,0.993537,0.092118,0.975462,0.0839602,-0.203532"
+    options[1] = "-DefaultZoom=1"
 
 for dat_file in sorted(glob.glob(sys.argv[1] + "*.mpd")):
     print "Processing:", dat_file
