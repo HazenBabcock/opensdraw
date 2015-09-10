47a22484c4f050ec868feb535335f7d0badbb548
Hazen Babcock <hbabcock@mac.com>
2015-09-09 21:32:38 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='OpenSDraw',
+      version='1.0',
+      description='A CAD program similar to OpenSCAD but for LEGO(R).',
+      author='Hazen Babcock',
+      author_email='hbabcock@mac.com',
+      packages=['opensdraw'],
+     )
