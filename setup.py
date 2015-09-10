73ebc05027e2a14ca6fe0d4d3ba329c79d3d4363
Hazen Babcock <hbabcock@mac.com>
2015-09-09 22:08:14 -0400

    Add install dependencies.

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 from distutils.core import setup
 
 setup(name='OpenSDraw',
-      version='1.0',
+      version='0.0.2',
       description='A CAD program similar to OpenSCAD but for LEGO(R).',
       author='Hazen Babcock',
       author_email='hbabcock@mac.com',
       packages=['opensdraw'],
+      install_requires['numpy', 'rply', 'scipy']
      )
