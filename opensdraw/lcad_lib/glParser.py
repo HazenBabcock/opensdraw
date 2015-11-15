880a6e63f2cd431ed7ca6103e64747d65bab54e6
Hazen Babcock <hbabcock@mac.com>
2015-11-15 08:40:24 -0500

    Initial work in adding lighting.

--- opensdraw/lcad_lib/glParser.py
+++ opensdraw/lcad_lib/glParser.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 """
-A parser for outputting OpenGL. This is based in part on this 
-example: https://gist.github.com/deepankarsharma/3494203
+A parser for outputting OpenGL. This is based in part on these web-pages:
+
+1. https://gist.github.com/deepankarsharma/3494203
+2. http://www.tomdalling.com/blog/modern-opengl/06-diffuse-point-lighting/
 
 This works for most parts, but it is not known to be completely
 conformant with the LDraw specification defined here:
@@ -13,48 +15,83 @@ In particular it has at least the following issues:
 2. Culling is disabled as I could not figure out how to get it
    to work properly.
 
-It is not designed to be fast as it is used mostly for static
-rendering.
+Also it is not that fast, but since it is mostly used for static
+images this is not too much of an issue.
 
 Hazen 11/15
-
 """
 
 import numpy
 
 from OpenGL import arrays, GL
 
+import colorsParser
 import datFileParser
-
 
 # Variables 
 
 # Vertex shader
 vertex = """
-#version 330
-in vec4 vin_position;
-in vec4 vin_color;
-out vec4 vout_color;
-uniform mat4 MVP;
+#version 150
+in vec4 vert;
+in vec4 vert_color;
+in vec4 vert_normal;
+
+out vec4 frag_color;
+out vec4 frag_normal;
+out vec4 frag_vert;
+
+uniform mat4 mvp;
 
 void main(void)
 {
-    vout_color = vin_color;
-    gl_Position = MVP * vin_position;
+    frag_color = vert_color;
+    frag_normal = vert_normal;
+    frag_vert = vert;
+
+    gl_Position = mvp * vert;
 }
 """
 
 # Fragment shader
 fragment = """
-#version 330
-in vec4 vout_color;
-out vec4 fout_color;
+#version 150
+
+uniform mat4 mvp;
+uniform vec3 light_position;
+uniform vec3 light_color;
+
+in vec4 frag_color;
+in vec4 frag_normal;
+in vec4 frag_vert;
+
+out vec4 final_color;
 
 void main(void)
 {
-    fout_color = vout_color;
+    //calculate normal in world coordinates
+    mat3 normal_matrix = transpose(inverse(mat3(mvp)));
+    vec3 normal = normalize(normal_matrix * vec3(frag_normal));
+    
+    //calculate the location of this fragment (pixel) in world coordinates
+    vec3 frag_position = vec3(mvp * frag_vert);
+    
+    //calculate the vector from this pixels surface to the light source
+    vec3 surface_to_light = light_position - frag_position;
+
+    //calculate the cosine of the angle of incidence
+    float brightness = dot(normal, surface_to_light) / (length(surface_to_light) * length(normal));
+    brightness = clamp(brightness, 0, 1);
+
+    //calculate final color of the pixel, based on:
+    // 1. The angle of incidence: brightness
+    // 2. The color/intensities of the light: light.intensities
+    final_color = vec4(brightness * light_color * frag_color.rgb, frag_color.a);
 }
 """
+
+all_colors = colorsParser.loadColors()
+
 
 class GLParserException(Exception):
     def __init__(self, message):
@@ -96,21 +133,31 @@ class GLParser(datFileParser.Parser):
     def addLine(self, p1, p2):
         self.vao_lines.addVertex(p1)
         self.vao_lines.addColor(self.edge_color)
+        
         self.vao_lines.addVertex(p2)
         self.vao_lines.addColor(self.edge_color)
-        
+
     def addTriangle(self, p1, p2, p3):
-        if self.lines_only:
-            self.addLine(p1, p2)
-            self.addLine(p2, p3)
-            self.addLine(p3, p1)            
-        else:
+        if not self.lines_only:
+            normal = numpy.cross(numpy.array(p2[:-1]) - numpy.array(p1[:-1]),
+                                 numpy.array(p3[:-1]) - numpy.array(p1[:-1]))
+            normal = normal.tolist() + [1.0]
+
             self.vao_triangles.addVertex(p1)
             self.vao_triangles.addColor(self.face_color)
+            self.vao_triangles.addNormal(normal)
+            
             self.vao_triangles.addVertex(p2)
-            self.vao_triangles.addColor(self.face_color)            
+            self.vao_triangles.addColor(self.face_color)
+            self.vao_triangles.addNormal(normal)
+            
             self.vao_triangles.addVertex(p3)
             self.vao_triangles.addColor(self.face_color)
+            self.vao_triangles.addNormal(normal)
+
+    def checkColor(self, color):
+        if (color != "16") and (color != "24"):
+            print "Unexpected color", color
 
     def command(self, parsed_line):
         if (len(parsed_line) > 1):
@@ -137,7 +184,7 @@ class GLParser(datFileParser.Parser):
                 #print "  ", self.depth, parsed_line, self.cw_winding
 
     def endFile(self):
-        self.vao_lines.finalize(GLParser.gl_shader)
+        #self.vao_lines.finalize(GLParser.gl_shader)
         self.vao_triangles.finalize(GLParser.gl_shader)
     
     def freeGL(self):
@@ -150,12 +197,22 @@ class GLParser(datFileParser.Parser):
             child.freeGL()
 
     def line(self, parsed_line):
+        self.checkColor(parsed_line[1])
         p1 = self.parsePoint(parsed_line[2:5])
         p2 = self.parsePoint(parsed_line[5:8])
         self.addLine(p1, p2)
 
     def newFile(self, parsed_line):
 
+        # Parse color
+        if (parsed_line[1] == "16"):
+            face_color = self.face_color
+            edge_color = self.edge_color
+        else:
+            color = all_colors[parsed_line[1]]
+            face_color = color.getFaceColor()
+            edge_color = color.getEdgeColor()
+            
         # Parse transformation matrix.
         [x, y, z, a, b, c, d, e, f, g, h, i] = map(float, parsed_line[2:14])
         matrix = numpy.array([[  a,   b,   c,   x], 
@@ -170,8 +227,8 @@ class GLParser(datFileParser.Parser):
         else:
             invert_winding = self.invert_winding
 
-        child = GLParser(self.face_color,
-                         self.edge_color,
+        child = GLParser(face_color,
+                         edge_color,
                          matrix = matrix,
                          invert_winding = invert_winding)
         self.invert_next = False
@@ -179,7 +236,7 @@ class GLParser(datFileParser.Parser):
         return child
 
     def optionalLine(self, parsed_line):
-        pass
+        self.checkColor(parsed_line[1])
 
     def parsePoint(self, point):
         point.append("1.0")
@@ -188,18 +245,20 @@ class GLParser(datFileParser.Parser):
         return pf.tolist()[:-1] + [1.0]
 
     def quadrilateral(self, parsed_line):
+        self.checkColor(parsed_line[1])
+
         p1 = self.parsePoint(parsed_line[2:5])
         p2 = self.parsePoint(parsed_line[5:8])
         p3 = self.parsePoint(parsed_line[8:11])
         p4 = self.parsePoint(parsed_line[11:14])
 
-        if self.lines_only:
+        if 0:
             self.addLine(p1, p2)
             self.addLine(p2, p3)
             self.addLine(p3, p4)
             self.addLine(p4, p1)
 
-        else:
+        if not self.lines_only:
             if self.cw_winding:
                 self.addTriangle(p1,p2,p3)
                 self.addTriangle(p1,p3,p4)
@@ -207,38 +266,49 @@ class GLParser(datFileParser.Parser):
                 self.addTriangle(p1,p3,p2)
                 self.addTriangle(p1,p4,p3)
 
-    def render(self, mvp):
+    def render(self, mvp, light_position, light_color):
 
         # Draw object.
         GL.glUseProgram(GLParser.gl_shader.program_id)
 
-        matrix_id = GLParser.gl_shader.uniformLocation('MVP')
+        mvp_id = GLParser.gl_shader.uniformLocation('mvp')
+        GL.glUniformMatrix4fv(mvp_id, 1, GL.GL_FALSE, mvp)
 
-        GL.glUniformMatrix4fv(matrix_id, 1, GL.GL_FALSE, mvp)
+        light_position_id = GLParser.gl_shader.uniformLocation('light_position')
+        GL.glUniform3fv(light_position_id, 1, light_position)
 
-        if not self.lines_only:
-            if (self.vao_triangles.v_size > 0):
-                GL.glBindVertexArray(self.vao_triangles.gl_id)
-                GL.glDrawArrays(self.vao_triangles.gl_type, 0, self.vao_triangles.v_size)
+        light_color_id = GLParser.gl_shader.uniformLocation('light_color')
+        GL.glUniform3fv(light_color_id, 1, light_color)
 
-        if (self.vao_lines.v_size > 0):
-            GL.glBindVertexArray(self.vao_lines.gl_id)
-            GL.glDrawArrays(self.vao_lines.gl_type, 0, self.vao_lines.v_size)
+        #if not self.lines_only:
+        #    if (self.vao_triangles.v_size > 0):
+        GL.glBindVertexArray(self.vao_triangles.gl_id)
+        GL.glDrawArrays(self.vao_triangles.gl_type, 0, self.vao_triangles.v_size)
+
+        #if (self.vao_lines.v_size > 0):
+        #    GL.glBindVertexArray(self.vao_lines.gl_id)
+        #    GL.glDrawArrays(self.vao_lines.gl_type, 0, self.vao_lines.v_size)
 
         GL.glBindVertexArray(0)
         GL.glUseProgram(0)
 
         # Draw children.
         for child in self.children:
-            child.render(mvp)
+            child.render(mvp, light_position, light_color)
                         
     def startFile(self, depth):
         self.depth = depth
 
     def triangle(self, parsed_line):
+        self.checkColor(parsed_line[1])
         p1 = self.parsePoint(parsed_line[2:5])
         p2 = self.parsePoint(parsed_line[5:8])
         p3 = self.parsePoint(parsed_line[8:11])
+
+        if 0:
+            self.addLine(p1, p2)
+            self.addLine(p2, p3)
+            self.addLine(p3, p1)
 
         if self.cw_winding:
             self.addTriangle(p1,p2,p3)
@@ -288,21 +358,27 @@ class GLShader(object):
 
 class GLVao(object):
 
-    def __init__(self, gl_type, color = None):
+    def __init__(self, gl_type):
 
-        self.color = color
-        self.gl_type = gl_type
-
-        self.c_size = 0
         self.gl_id = 0
-        self.vertices = []
-        self.colors = []
+        self.gl_type = gl_type
         self.vbo_id = 0
+        
+        self.c_size = 0
+        self.n_size = 0
         self.v_size = 0
+        
+        self.colors = []
+        self.normals = []
+        self.vertices = []
 
     def addColor(self, color):
         self.colors.extend(color)
         self.c_size += 4
+
+    def addNormal(self, normal):
+        self.normals.extend(normal)
+        self.n_size += 4
         
     def addVertex(self, vertex):
         self.vertices.extend(vertex)
@@ -315,14 +391,18 @@ class GLVao(object):
         if (self.c_size != self.v_size):
             raise GLParserException("Number of colors vertices does not match number of position vertices")
 
+        if (self.c_size != self.n_size):
+            raise GLParserException("Number of normal vertices does not match number of position vertices")
+
         color_data = numpy.array(self.colors, dtype = numpy.float32)
+        normal_data = numpy.array(self.normals, dtype = numpy.float32)
         vertex_data = numpy.array(self.vertices, dtype = numpy.float32)
         
         self.gl_id = GL.glGenVertexArrays(1)
         GL.glBindVertexArray(self.gl_id)
 
         # Create buffers.
-        self.vbo_id = GL.glGenBuffers(2)
+        self.vbo_id = GL.glGenBuffers(3)
 
         # Fill first buffer with the vertices.
         GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo_id[0])
@@ -330,7 +410,7 @@ class GLVao(object):
                         arrays.ArrayDatatype.arrayByteCount(vertex_data), 
                         vertex_data, 
                         GL.GL_STATIC_DRAW)
-        GL.glVertexAttribPointer(gl_shader.attributeLocation('vin_position'), 
+        GL.glVertexAttribPointer(gl_shader.attributeLocation('vert'), 
                                  4,
                                  GL.GL_FLOAT,
                                  GL.GL_FALSE,
@@ -344,19 +424,33 @@ class GLVao(object):
                         arrays.ArrayDatatype.arrayByteCount(color_data), 
                         color_data,
                         GL.GL_STATIC_DRAW)
-        GL.glVertexAttribPointer(gl_shader.attributeLocation('vin_color'), 
+        GL.glVertexAttribPointer(gl_shader.attributeLocation('vert_color'), 
                                  4,
                                  GL.GL_FLOAT,
                                  GL.GL_FALSE,
                                  0,
                                  None)
         GL.glEnableVertexAttribArray(1)
+
+        # Fill third buffer with the normals.
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo_id[2])
+        GL.glBufferData(GL.GL_ARRAY_BUFFER,
+                        arrays.ArrayDatatype.arrayByteCount(color_data), 
+                        color_data,
+                        GL.GL_STATIC_DRAW)
+        GL.glVertexAttribPointer(gl_shader.attributeLocation('vert_normal'),
+                                 4,
+                                 GL.GL_FLOAT,
+                                 GL.GL_FALSE,
+                                 0,
+                                 None)
+        GL.glEnableVertexAttribArray(2)
         
         GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
         GL.glBindVertexArray(0)
 
     def freeBuffers(self):
-        GL.glDeleteBuffers(2, self.vbo_id)
+        GL.glDeleteBuffers(len(self.vbo_id), self.vbo_id)
         GL.glDeleteVertexArrays(1, [self.gl_id])
         self.v_size = 0
 
